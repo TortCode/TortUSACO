cf353eec03f087f8ce5ea3e31c97fbeb8559aec5
TortTech <44101297+TortTech@users.noreply.github.com>
2018-11-30 17:51:07 -0500

    Update Main.cpp

--- Crypt1/Main.cpp
+++ Crypt1/Main.cpp
@@ -5,10 +5,9 @@ LANG: C++
 */
 #include <fstream>
 #include <string>
-#include <vector>
-#define useproperchar(remainder) if (!(remainder == a || remainder == b || remainder == c || remainder == d || remainder == e)) { \
-isSolution = false; \
-goto fail;}
+#include <cmath>
+#include <algorithm>
+#define ISPROPERDIG(x) if (find(digits, digits + N, x) == (digits + N)) goto fail; //if no match for x in digits, FAIL
 
 using namespace std;
 
@@ -17,63 +16,66 @@ int main()
 	ifstream fin("crypt1.in");
 	ofstream fout("crypt1.out");
 	int N;
-	vector<int> digits;
+	int digits[10];
 	fin >> N;
-	int num;
 	for (int i = 0; i < N; i++) {
-		fin >> num;
-		digits.push_back(num);
+		fin >> digits[i];
 	}
 	fin.close();
 	/*
 	 abc
 	x de
 	____
-	 ***
-	***
+	 *** pp1
+	***  pp2
 	----
-	****
+	**** sum
 	 */
 	int solutions = 0;
-	for (int a : digits) {
-		for (int b : digits) {
-			for (int c : digits) {
-				for (int d : digits) {
-					for (int e : digits) {
-						int abc = 100 * a + 10 * b + c;
-						int pp1 = abc * e; //partial product 1
-						if (pp1 > 999) continue;
-						int pp2 = abc * d; //partial product 2
-						if (pp2 > 999) continue;
-						int sum = 10 * pp2 + pp1;
-						if (sum > 9999) continue;
-						bool isSolution = true;
-						int remainder;
-						for (int i = 0; i < 3; i++) {
-							remainder = pp1 % 10;
-							pp1 /= 10;
-							useproperchar(remainder);
-						}
-						for (int i = 0; i < 3; i++) {
-							remainder = pp2 % 10;
-							pp2 /= 10;
-							useproperchar(remainder);
-						}
-						for (int i = 0; i < 4; i++) {
-							remainder = sum % 10;
-							sum /= 10;
-							useproperchar(remainder);
-						}
-					fail:
-						if (!isSolution) continue;
-						else solutions++;
+	for (int i = 0; i < pow(N, 5); i++) {
+		int test = i;	//N-ary notation for test case
+		int a = digits[test % N];
+		test /= N;
+		int b = digits[test % N];
+		test /= N;
+		int c = digits[test % N];
+		test /= N;
+		int d = digits[test % N];
+		test /= N;
+		int e = digits[test % N];
 
-					}
-				}
+		int abc = 100 * a + 10 * b + c;
+		int pp1 = abc * e; //partial product 1
+		int pp2 = abc * d; //partial product 2
+		int sum = 10 * pp2 + pp1;
+
+		if (pp1 > 999) goto fail;
+		if (pp2 > 999) goto fail;
+		if (sum > 9999) goto fail;
+		{
+			int remainder;
+			int ct;
+			for (ct = 0; ct < 3; ct++) {
+				remainder = pp1 % 10;
+				pp1 /= 10;
+				ISPROPERDIG(remainder)
+			}
+			for (ct = 0; ct < 3; ct++) {
+				remainder = pp2 % 10;
+				pp2 /= 10;
+				ISPROPERDIG(remainder)
+			}
+			for (ct = 0; ct < 4; ct++) {
+				remainder = sum % 10;
+				sum /= 10;
+				ISPROPERDIG(remainder)
 			}
 		}
+		//if no fail
+		solutions++;
+	fail:;
 	}
 
-	fout << solutions;
+	fout << solutions << endl;
 	fout.close();
 }
