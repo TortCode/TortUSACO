f0ba2dbf01cba4ec6f2e2fd0558dad75496cf3ac
TortTech <44101297+TortTech@users.noreply.github.com>
2018-12-01 12:09:03 -0500

    Add files via upload

--- Crypt1/Main.cpp
+++ Crypt1/Main.cpp
@@ -4,22 +4,23 @@ PROG: crypt1
 LANG: C++
 */
 #include <fstream>
-#include <string>
 #include <cmath>
 #include <algorithm>
-#define ISPROPERDIG(x) if (find(digits, digits + N, x) == (digits + N)) goto fail; //if no match for x in digits, FAIL
+
+#define EXTRACTandCHECK_DIGIT(n) curDigit = n % 10; n /= 10; if (!isProperDigit[curDigit]) goto FAIL;
 
 using namespace std;
 
 int main()
 {
+	int N, digits[9];
+	bool isProperDigit[10] = { false };
+
 	ifstream fin("crypt1.in");
-	ofstream fout("crypt1.out");
-	int N;
-	int digits[10];
 	fin >> N;
 	for (int i = 0; i < N; i++) {
 		fin >> digits[i];
+		isProperDigit[digits[i]] = true;
 	}
 	fin.close();
 	/*
@@ -28,54 +29,35 @@ int main()
 	____
 	 *** pp1
 	***  pp2
-	----
 	**** sum
 	 */
 	int solutions = 0;
-	for (int i = 0; i < pow(N, 5); i++) {
-		int test = i;	//N-ary notation for test case
-		int a = digits[test % N];
-		test /= N;
-		int b = digits[test % N];
-		test /= N;
-		int c = digits[test % N];
-		test /= N;
-		int d = digits[test % N];
-		test /= N;
-		int e = digits[test % N];
+	const int permutations = pow(N, 5);
+	for (int i = 0, test = 0; i < permutations; test = ++i) {
+		int a, b, c, d, e;	//N-ary notation for test permutation
+		a = digits[test % N]; test /= N;
+		b = digits[test % N]; test /= N;
+		c = digits[test % N]; test /= N;
+		d = digits[test % N]; test /= N;
+		e = digits[test % N];
 
-		int abc = 100 * a + 10 * b + c;
-		int pp1 = abc * e; //partial product 1
-		int pp2 = abc * d; //partial product 2
-		int sum = 10 * pp2 + pp1;
-
-		if (pp1 > 999) goto fail;
-		if (pp2 > 999) goto fail;
-		if (sum > 9999) goto fail;
+		int abc, pp1, pp2, sum;
+		abc = 100 * a + 10 * b + c;
+		pp1 = abc * e; if (pp1 > 999) goto FAIL; //Calculation and Digit Length Check
+		pp2 = abc * d; if (pp2 > 999) goto FAIL;
+		sum = 10 * pp2 + pp1; if (sum > 9999) goto FAIL;
+		//brackets circumvent GOTO init skipping
 		{
-			int remainder;
+			int curDigit; //for operation of EXTRACTandCHECK_DIGIT
 			int ct;
-			for (ct = 0; ct < 3; ct++) {
-				remainder = pp1 % 10;
-				pp1 /= 10;
-				ISPROPERDIG(remainder)
-			}
-			for (ct = 0; ct < 3; ct++) {
-				remainder = pp2 % 10;
-				pp2 /= 10;
-				ISPROPERDIG(remainder)
-			}
-			for (ct = 0; ct < 4; ct++) {
-				remainder = sum % 10;
-				sum /= 10;
-				ISPROPERDIG(remainder)
-			}
+			for (ct = 0; ct < 3; ct++) { EXTRACTandCHECK_DIGIT(pp1) }
+			for (ct = 0; ct < 3; ct++) { EXTRACTandCHECK_DIGIT(pp2) }
+			for (ct = 0; ct < 4; ct++) { EXTRACTandCHECK_DIGIT(sum) }
 		}
-		//if no fail
-		solutions++;
-	fail:;
+		solutions++; //if no FAIL
+	FAIL:;
 	}
-
+	ofstream fout("crypt1.out");
 	fout << solutions << endl;
 	fout.close();
 }
