0f570bab7e882ee4807c4c894a01d1fb680e8490
TortCode <44101297+TortCode@users.noreply.github.com>
2018-12-02 08:52:02 -0500

    Update Main.cpp

--- Beads/Main.cpp
+++ Beads/Main.cpp
@@ -19,32 +19,30 @@ int main() {
 	int max = 0;
 	int s1, s2; //ends of side1 and side2
 	char clr; //clr searched for {r,b,w}
+	
 	beads = beads + beads; //duplication creates pseudo-circular string
-
-	for (int i = 1; i < N * 2;) {  //skip thru split pt's P(x)
+	int len = N * 2;
+	for (int i = 1; i < len; i += s1) {  //go to end of fo
 		s2 = 0;
 		s1 = 0;
+		
 		clr = beads[i];
-		for (int j = i; j < N * 2;) { //find longest segment going clockwise
-			if (clr == 'w') clr = beads[j]; //if nonwhite found, clr will stay nonwhite
-			if (beads[j] == clr || beads[j] == 'w') {
-				s1++;
-				j++;
-			}
+		for (int j = i; j < len; j++) { //forwards
+			if (clr == 'w') clr = beads[j];
+			//color indeterminate('w') until nonwhite found
+			if (beads[j] == clr || beads[j] == 'w') s1++;
 			else break;
 		}
+		
 		clr = beads[i - 1];
-		for (int j = i - 1; j >= 0;) { //find longest segment going clockwise
-			if (clr == 'w') clr = beads[j]; //if nonwhite found, clr will stay nonwhite
-			if (beads[j] == clr || beads[j] == 'w') {
-				s2++;
-				j--;
-			}
+		for (int j = i - 1; j >= 0; j--) { //backwards
+			if (clr == 'w') clr = beads[j];
+			//color indeterminate('w') until nonwhite found
+			if (beads[j] == clr || beads[j] == 'w') s2++;
 			else break;
 		}
 
 		max = s1 + s2 > max ? s1 + s2 : max; //get new max
-		i += s1; //go to end of segment
 	}
 	if (max > N) max = N;
 	ofstream output("beads.out");
