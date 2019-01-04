edd90d3a990753d792193984f48aa908fd551f2c
TortCode <44101297+TortCode@users.noreply.github.com>
2019-01-04 10:39:01 -0500

    Update ariprog.cpp

--- Section1.5/ariprog.cpp
+++ Section1.5/ariprog.cpp
@@ -9,9 +9,16 @@ LANG: C++
 
 int N, M;
 
-bool searchValue(const std::vector<int>& vec, int start, int target)
+inline bool searchValue(const std::vector<int>& vec, int start, int target)
 {
-	return true;
+	int i;
+	for (i = start; i < vec.size(); i++) {
+		if (vec[i] == target)
+			return true;
+		if (vec[i] > target)
+			return false;
+	}
+	return false;
 }
 
 int main()
@@ -20,11 +27,14 @@ int main()
 	input >> N >> M;
 	input.close();
 
-	std::vector<int> bisquares;
+	std::vector<int> bisquares(1);
 
+	int sqSum;
 	for (int p = 0; p <= M; p++) {
 		for (int q = 0; q <= p; q++) {
-			bisquares.push_back(p*p + q * q);
+			sqSum = p * p + q * q;
+			if (!searchValue(bisquares, 0, sqSum))
+				bisquares.push_back(sqSum);
 		}
 	}
 
