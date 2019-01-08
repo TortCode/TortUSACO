aaa66e9518367158f310175f793841f8d853f965
TortCode <44101297+TortCode@users.noreply.github.com>
2019-01-08 11:20:37 -0500

    Update ariprog.cpp

--- Section1.5/ariprog.cpp
+++ Section1.5/ariprog.cpp
@@ -4,40 +4,64 @@ PROG: ariprog
 LANG: C++
 */
 #include <fstream>
-#include <iostream>
+#include <algorithm>
 #include <vector>
 
-int N, M;
+int N, M; //len of seq; parameter for set of sum of squares
 
-inline bool searchValue(const std::vector<int>& vec, int start, int target)
+std::vector<int> bisquares(1);
+
+inline bool searchValue(int start, int target)
 {
-	int i;
-	for (i = start; i < vec.size(); i++) {
-		if (vec[i] == target)
+	for (int i = start; i < bisquares.size(); i++) {
+		if (bisquares[i] == target)
 			return true;
-		if (vec[i] > target)
-			return false;
 	}
 	return false;
+}
+inline bool seqExists(int start, int a, int b)
+{
+	for (int n = 0; n < N; n++) {
+		if (!searchValue(start, a + n * b))
+			return false;
+	}
+	return true;
 }
 
 int main()
 {
 	std::ifstream input("ariprog.in");
+	std::ofstream output("ariprog.out");
 	input >> N >> M;
 	input.close();
 
-	std::vector<int> bisquares(1);
-
 	int sqSum;
-	for (int p = 0; p <= M; p++) {
+	for (int p = 0; p <= M; p++) { //generate squareSums
 		for (int q = 0; q <= p; q++) {
 			sqSum = p * p + q * q;
-			if (!searchValue(bisquares, 0, sqSum))
+			if (!searchValue(0, sqSum))
 				bisquares.push_back(sqSum);
 		}
 	}
 
+	int size = bisquares.size();
+	sort(bisquares.begin(), bisquares.end());
 
+	int a, b, sol = 0;
+	int maxB = (bisquares[size - 1] - bisquares[0]) / (N - 1);
+	for (int b = 1; b <= maxB; b++) {
+		for (int i = 0; i < size - N; i++) {
+			a = bisquares[i];
+			if (seqExists(i, a, b)) {
+				output << a << " " << b << std::endl;
+				sol++;
+			}
+		}
+	}
+
+	if (sol == 0)
+		output << "NONE" << std::endl;
+	
+	output.close();
 
 }
