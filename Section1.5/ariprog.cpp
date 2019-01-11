73178d1337626cdd2006700daf4013c063af8714
TortCode <44101297+TortCode@users.noreply.github.com>
2019-01-11 16:29:26 -0500

    Update ariprog.cpp

--- Section1.5/ariprog.cpp
+++ Section1.5/ariprog.cpp
@@ -8,25 +8,28 @@ LANG: C++
 #include <vector>
 
 int N, M; //len of seq; parameter for set of sum of squares
+std::vector<int> sumsquares(1);
 
-std::vector<int> bisquares(1);
-
+int loc;
 inline bool searchValue(int start, int target)
 {
-	for (int i = start; i < bisquares.size(); i++) {
-		if (bisquares[i] == target)
+	for (loc = start; loc < sumsquares.size(); loc++) {
+		if (sumsquares[loc] == target)
 			return true;
+		if (sumsquares[loc] > target)
+			return false;
 	}
 	return false;
 }
-inline bool seqExists(int start, int a, int b)
+/*inline bool seqExists(int start, int a, int b)
 {
+	loc = start;
 	for (int n = 0; n < N; n++) {
-		if (!searchValue(start, a + n * b))
+		if (!searchValue(loc, a + n * b))
 			return false;
 	}
 	return true;
-}
+}*/
 
 int main()
 {
@@ -35,24 +38,32 @@ int main()
 	input >> N >> M;
 	input.close();
 
-	int sqSum;
 	for (int p = 0; p <= M; p++) { //generate squareSums
 		for (int q = 0; q <= p; q++) {
-			sqSum = p * p + q * q;
-			if (!searchValue(0, sqSum))
-				bisquares.push_back(sqSum);
+			sumsquares.push_back(p * p + q * q);
 		}
 	}
 
-	int size = bisquares.size();
-	sort(bisquares.begin(), bisquares.end());
-
+	sort(sumsquares.begin(), sumsquares.end());
+	for (int i = sumsquares.size() - 1; i > 0; i--) {
+		if (sumsquares[i] == sumsquares[i - 1])
+			sumsquares.erase(sumsquares.begin() + i);
+	}
+	int size = sumsquares.size();
+	
 	int a, b, sol = 0;
-	int maxB = (bisquares[size - 1] - bisquares[0]) / (N - 1);
+	int maxB = (sumsquares[size - 1] - sumsquares[0]) / (N - 1);
+	bool isSol;
 	for (int b = 1; b <= maxB; b++) {
 		for (int i = 0; i < size - N; i++) {
-			a = bisquares[i];
-			if (seqExists(i, a, b)) {
+			a = sumsquares[i];
+			isSol = true;
+			loc = i;
+			for (int n = 0; n < N; n++) {
+				if (!searchValue(loc, a + n * b))
+					isSol = false;
+			}
+			if (isSol) {
 				output << a << " " << b << std::endl;
 				sol++;
 			}
@@ -61,7 +72,7 @@ int main()
 
 	if (sol == 0)
 		output << "NONE" << std::endl;
-	
+
 	output.close();
 
 }
