baa7f6e5461f1074f23b2f13bcc235c865e9fdd8
TortCode <44101297+TortCode@users.noreply.github.com>
2019-01-13 08:44:49 -0500

    Update ariprog.cpp

--- Section1.5/ariprog.cpp
+++ Section1.5/ariprog.cpp
@@ -10,10 +10,11 @@ LANG: C++
 
 int N, M; //len of seq; parameter for set of bisquares
 int maxBisquare;
+
 std::vector<std::pair<int, int>> sequences;
 std::bitset<125001> bisquares; //if bisquares[i] then i is bisquare
 
-inline bool seqExists(const int& a, const int& b)
+bool seqExists(const int& a, const int& b)
 {
 	int term = a;
 	for (int n = 1; n < N; n++) {
@@ -30,18 +31,17 @@ int main()
 	input >> N >> M;
 	input.close();
 
-	maxBisquare = 2 * M * M;
-	//generate bisquares
 	for (int p = 0; p <= M; p++) {
 		for (int q = 0; q <= p; q++) {
 			bisquares[p * p + q * q] = true;
 		}
 	}
-	
+
+	maxBisquare = 2 * M * M;
 	const int maxB = maxBisquare / (N - 1);
-	for (unsigned int a = 0; a <= maxBisquare; a++) {
+	for (int a = 0; a <= maxBisquare; a++) {
 		if (bisquares[a])
-			for (unsigned int j = a + 1; j <= maxBisquare; j++) {
+			for (int j = a + 1; j <= maxBisquare; j++) {
 				if (bisquares[j]) {
 					int b = j - a;
 					if (b > maxB) break;
@@ -59,6 +59,7 @@ int main()
 		sort(sequences.begin(), sequences.end(), [](std::pair<int, int> x, std::pair<int, int> y) {
 			return (x.second == y.second) ? x.first < y.first : x.second < y.second;
 		});
+
 		for (std::pair<int, int> solution : sequences)
 			output << solution.first << " " << solution.second << std::endl;
 	}
