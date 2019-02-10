c7956b00b51bec468cd912e087fdd5b9f16bbf92
TortCode <44101297+TortCode@users.noreply.github.com>
2019-02-10 16:24:51 -0500

    Add files via upload

---
+++ Section1.2/ride.java
@@ -0,0 +1,28 @@
+/* 
+ID: teerth.1
+LANG: JAVA
+TASK: ride
+ */
+import java.io.*;
+public class ride {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("ride.in"));
+		String comet = br.readLine();
+		String group = br.readLine();
+		br.close();
+		int cometProduct = 1, groupProduct = 1;
+		for (int i = 0; i < comet.length(); i++)
+			cometProduct *= comet.charAt(i) - 'A' + 1;
+		for (int i = 0; i < group.length(); i++)
+			groupProduct *= group.charAt(i) - 'A' + 1;
+
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+		if (cometProduct%47==groupProduct%47)
+			pw.println("GO");
+		else
+			pw.println("STAY");
+		pw.close();
+	}
+
+}
