1713f2e6c7f782f476e28a44a970aa674e3cdee9
TortCode <44101297+TortCode@users.noreply.github.com>
2019-02-15 17:37:52 -0500

    Update helpcross.cpp

--- helpcross.cpp
+++ helpcross.cpp
@@ -24,20 +24,19 @@ int main()
 
 	sort(chickens, chickens + C);
 	sort(cows, cows + N, [](Cow l, Cow r) {
-		if (l.start < r.start)
+		if (l.fin < r.fin)
 			return true;
-		if (l.start == r.start)
-			return l.fin < r.fin;
+		if (l.fin == r.fin)
+			return l.start < r.start;
 		return false;
 	});
 
 	int pairs = 0;
-	int cik = 0;
 	for (int i = 0; i < N; i++) {
-		if (cik >= C) break;
-		while (chickens[cik] < cows[i].start) cik++;
+		int cik = 0;
+		while (cik < C && chickens[cik] < cows[i].start) cik++;
 		if (cik < C && chickens[cik] <= cows[i].fin) {
-			cik++;
+			chickens[cik] = 0;
 			pairs++;
 		}
 	}
