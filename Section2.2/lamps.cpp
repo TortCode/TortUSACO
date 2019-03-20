5bad7d04eade484916b1293190309d1af8f189c4
TortCode <44101297+TortCode@users.noreply.github.com>
2019-03-20 11:05:07 -0400

    Add files via upload

--- Section2.2/lamps.cpp
+++ Section2.2/lamps.cpp
@@ -13,38 +13,41 @@ struct State {
 	int ct = 0;
 	State(int sz)
 	{
-		lamps.resize(sz + 1);
-		for (bool b : lamps) b = 1;
+		lamps.resize(sz, true);
+	}
+	State button1()
+	{
+		State st = *this;
+		st.lamps.flip();
+		st.ct++;
+		return st;
+	}
+	State button2()
+	{
+		State st = *this;
+		int len = st.lamps.size();
+		for (int i = 1; i < len; i += 2) st.lamps[i-1] = !st.lamps[i-1];
+		st.ct++;
+		return st;
+	}
+	State button3()
+	{
+		State st = *this;
+		int len = st.lamps.size();
+		for (int i = 2; i < len; i += 2) st.lamps[i-1] = !st.lamps[i-1];
+		st.ct++;
+		return st;
+	}
+	State button4()
+	{
+		State st = *this;
+		int len = st.lamps.size();
+		for (int i = 1; i < len; i += 3) st.lamps[i-1] = !st.lamps[i-1];
+		st.ct++;
+		return st;
 	}
 };
 
-State& button1(State st)
-{
-	st.lamps.flip();
-	st.ct++;
-	return st;
-}
-State& button2(State st)
-{
-	int len = st.lamps.size();
-	for (int i = 1; i < len; i += 2) st.lamps[i] = !st.lamps[i];
-	st.ct++;
-	return st;
-}
-State& button3(State st)
-{
-	int len = st.lamps.size();
-	for (int i = 2; i < len; i += 2) st.lamps[i] = !st.lamps[i];
-	st.ct++;
-	return st;
-}
-State& button4(State st)
-{
-	int len = st.lamps.size();
-	for (int i = 1; i < len; i += 3) st.lamps[i] = !st.lamps[i];
-	st.ct++;
-	return st;
-}
 
 int main()
 {
@@ -55,14 +58,27 @@ int main()
 	ofstream output("lamps.out");
 	input >> N >> C;
 	poss.push_back(State(N));
-	for (int i; i != -1; input >> i) on.push_back(i);
-	for (int i; i != -1; input >> i) off.push_back(i);
+	int loc;
+	input >> loc;
+	while (loc != -1) {
+		on.push_back(loc);
+		input >> loc;
+	}
+	input >> loc;
+	while (loc != -1) {
+		off.push_back(loc);
+		input >> loc;
+	}
 
-	for (State st : poss) poss.push_back(button1(st));
-	for (State st : poss) poss.push_back(button2(st));
-	for (State st : poss) poss.push_back(button3(st));
-	for (State st : poss) poss.push_back(button4(st));
-	
+	for (int i = poss.size() - 1; i >= 0; i--)
+		poss.push_back(poss[i].button1());
+	for (int i = poss.size() - 1; i >= 0; i--)
+		poss.push_back(poss[i].button2());
+	for (int i = poss.size() - 1; i >= 0; i--)
+		poss.push_back(poss[i].button3());
+	for (int i = poss.size() - 1; i >= 0; i--)
+		poss.push_back(poss[i].button4());
+
 	for (int i = poss.size() - 1; i >= 0; i--) {
 		bool remove = false;
 		if (poss[i].ct % 2 != C % 2)
@@ -70,14 +86,24 @@ int main()
 		if (poss[i].ct > C)
 			remove = true;
 		for (int pos : on)
-			if (!poss[i].lamps[pos]) remove = true;
+			if (!(poss[i].lamps[pos-1])) remove = true;
 		for (int pos : off)
-			if (poss[i].lamps[pos]) remove = true;
+			if (poss[i].lamps[pos-1]) remove = true;
 		if (remove)
 			poss.erase(poss.begin() + i);
 	}
+	for (int i = poss.size() - 1; i >= 0; i--)
+		for (int j = i - 1; j >= 0; j--)
+			if (poss[i].lamps == poss[j].lamps)
+				poss.erase(poss.begin() + j);
 
-	sort(poss.begin(), poss.end(), [&](State lhs, State rhs) {
-
-	});
+	sort(poss.begin(), poss.end(), [](State lhs, State rhs) {return lhs.lamps < rhs.lamps;});
+	for (State st : poss) {
+		for (bool b : st.lamps)
+			output << b;
+		output << '\n';
+	}
+	if (poss.size() == 0)
+		output << "IMPOSSIBLE" << '\n';
+	output.close();
 }
