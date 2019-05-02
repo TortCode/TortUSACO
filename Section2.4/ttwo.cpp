8f2c8f0c1849c274c78f22b6f6e8dcafd8f17ddb
TortCode <44101297+TortCode@users.noreply.github.com>
2019-05-02 11:29:43 -0400

    Create ttwo.cpp

---
+++ Section2.4/ttwo.cpp
@@ -0,0 +1,23 @@
+/*
+ID: teerth.1
+PROG: ttwo
+LANG: C++
+*/
+#include <fstream>
+#include <string>
+#define SIZE 10
+using namespace std;
+
+int main()
+{
+  ifstream input("ttwo.in");
+  ofstream output("ttwo.out");
+  string s;
+  for (int i = 0; i < SIZE; i++) {
+    input >> s;
+    for (int j = 0; j < SIZE; j++) {
+    
+    }
+  }
+  
+}
