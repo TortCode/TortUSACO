d01bd1b3595a289ef1a835717af8725a2e2af1f0
TortCode <44101297+TortCode@users.noreply.github.com>
2019-11-01 19:00:14 -0400

    Update kimbits.cpp

--- Section3.2/kimbits.cpp
+++ Section3.2/kimbits.cpp
@@ -14,17 +14,17 @@ int pos[32]; // position of 1s
 int generate(){
 {
 	int combo[32][32];
-	combo[0][0] = 1;
 	int n, k;
 	for (n = 0; n <= 31; n++) {
 		combo[n][0] = 1;
 		combo[n][n] = 1;
-		numstrings[n][0] = 1;
-		for (k = 1; k < n; k++) {
+		for (k = 1; k < n; k++)
 			combo[n][k] = combo[n - 1][k - 1] + combo[n - 1][k];
-			numstrings[n][k] = numstrings[n][k - 1] + combo[n][k];
-		}
-		numstrings[n][n] = numstrings[n][n - 1] + 1;
+	}
+	for (n = 0; n <= 31; n++) {
+		numstrings[n][0] = 1;
+		for (k = 1; k <= n; k++)
+			numstrings[n][k] = numstrings[n][k-1] + combo[n][k];
 	}
 }
 	
@@ -50,15 +50,28 @@ int main()
 
 	generate();
 	
-	int index = I;
-	for(int i = 0, k = 0, len = L; index != 1; len--, i++) {
-		k = 0;
-		while (index > numstrings[k][len]) k++;
+	for(int index = I, i = 0, bits = L; index != 1; bits--, i++) {
+		int k = 0;
+		while (index > numstrings[k][bits]) k++;
 		pos[i] = k;
-		index -= numstrings[k-1][len];
+		index -= numstrings[k-1][bits];
+	}
+	
+	string ret;
+	int strindex = 1;
+	for (int i = 0; i < 32 && pos[i] != 0; i++) {
+		while(strindex < pos[i]) {
+			ret = "0" + ret;
+			strindex++;
+		}
+		ret = "1" + ret;
+		strindex++;
+	}
+	while(strindex <= N) {
+		ret = "0" + ret;
+		strindex++;
 	}
 
-
-	output << toBinaryString(ret) << endl;
+	output << ret << endl;
 	output.close();
 }
