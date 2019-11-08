d7d8467f774f0f70b2036ee6a599707004e1da67
TortCode <44101297+TortCode@users.noreply.github.com>
2019-11-08 15:45:38 -0500

    Add files via upload

--- Section3.2/spin.cpp
+++ Section3.2/spin.cpp
@@ -9,7 +9,14 @@ using namespace std;
 
 struct Wheel {
 	int speed;
+	int rotation = 0;
 	bitset<360> wedges;
+	void rotate() {
+		rotation = (rotation + speed) % 360;
+	}
+	bool getBit(int angle) {
+		return wedges[(angle - rotation + 360) % 360];
+	}
 	void setBits(int start, int end)
 	{
 		for (int i = start; i <= end; i++)
@@ -17,11 +24,36 @@ struct Wheel {
 	}
 } wheels[5];
 
+int findAlignment() {
+	int t = 0;
+	int angle;
+	do {
+		for (angle = 0; angle < 360; angle++) {
+			if (wheels[0].getBit(angle)
+				&& wheels[1].getBit(angle)
+				&& wheels[2].getBit(angle)
+				&& wheels[3].getBit(angle)
+				&& wheels[4].getBit(angle))
+				return t;
+		}
+		wheels[0].rotate();
+		wheels[1].rotate();
+		wheels[2].rotate();
+		wheels[3].rotate();
+		wheels[4].rotate();
+		t++;
+	} while (!(wheels[0].rotation == 0
+		&& wheels[1].rotation == 0
+		&& wheels[2].rotation == 0
+		&& wheels[3].rotation == 0
+		&& wheels[4].rotation == 0));
+	return -1;
+}
+
 int main() {
 	ifstream input("spin.in");
 	ofstream output("spin.out");
-
-	for (int i = 0; i < 5; i++){
+	for (int i = 0; i < 5; i++) {
 		int numwedge;
 		int start, extent;
 		input >> wheels[i].speed >> numwedge;
@@ -30,10 +62,9 @@ int main() {
 			wheels[i].setBits(start, start + extent);
 		}
 	}
-
-	int rotation[5]{ 0 };
-
-
-
-	return 0;
+	int result = findAlignment();
+	if (result == -1)
+		output << "none" << endl;
+	else
+		output << result << endl;
 }
