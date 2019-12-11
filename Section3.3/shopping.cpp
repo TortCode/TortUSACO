3d0fb8e56d30ac43505456fe99503f1841b5bedc
TortCode <44101297+TortCode@users.noreply.github.com>
2019-12-11 13:50:00 -0500

    Add files via upload

--- Section3.3/shopping.cpp
+++ Section3.3/shopping.cpp
@@ -5,71 +5,87 @@ LANG: C++
 */
 #include <fstream>
 #include <vector>
-#include <utility>
+#include <algorithm>
+#include <cmath>
 
-// 0 to 5 for each item
-constexpr int MAXCOMBOCODE = 6 * 6 * 6 * 6 * 6;
-int minPrice[MAXCOMBOCODE];
-// from code to index in minprice
-int index[1000];
-int sixpow[]{ 1,6,36,36 * 6,36 * 36 };
 int noffers, nbuys;
 
-// code, no of items
-using Item = std::pair<int, int>;
 struct Offer {
-	std::vector<Item> items;
+	int items[5]{};
 	int price;
-	int encode()
-	{
-		int n;
-		for (Item it : items)
-			n += sixpow[index[it.first]] * it.second;
-		return n;
-	}
-} offers[99];
+};
+std::vector<Offer> offers;
+// product code map-> 1-5
+int index[1000];
+int count = 0;
+int purchase[5];
 
-struct Purchase {
-	int code;
-	int nitem;
-	int price;
-} purchases[5];
-
-
-
-void dp()
+int minprice[6][6][6][6][6];
+bool isValid(int a, int b, int c, int d, int e)
 {
-	for (int i = 0; i < noffers; ++i) {
-		int code = offers[i].encode();
-		int p = offers[i].price;
-		for (int j = MAXCOMBOCODE - 1; j >= 0; --j) {
-
+	return a <= 5
+		&& b <= 5
+		&& c <= 5
+		&& d <= 5
+		&& e <= 5;
+}
+int dp()
+{
+	int sixpow = std::pow(6, nbuys);
+	int a, b, c, d, e;
+	int na, nb, nc, nd, ne;
+	for (Offer o : offers) {
+		for (int i = 0; i < sixpow; ++i) {
+			a = i % 6; i /= 6;
+			b = i % 6; i /= 6;
+			c = i % 6; i /= 6;
+			d = i % 6; i /= 6;
+			e = i % 6; i /= 6;
+			na = a + o.items[0];
+			nb = b + o.items[1];
+			nc = c + o.
+			if ()
 		}
 	}
-}
 
+}
 int main()
 {
 	std::ifstream input("shopping.in");
 	std::ofstream output("shopping.out");
+
 	input >> noffers;
 	int n, c, k, p;
+	// the count of index is assigned in order of scanning product code
+	std::fill(index, index + 1000, -1);
 	for (int i = 0; i < noffers; ++i) {
 		input >> n;
+		Offer o;
 		for (int j = 0; j < n; ++j) {
 			input >> c >> k;
-			offers[i].items.push_back(Item(c, k));
+			if (index[c] == -1) {
+				index[c] = count; ++count;
+			}
+			o.items[index[c]] = k;
 		}
-		input >> offers[n].price;
+		input >> o.price;
+		offers.push_back(o);
 	}
 	input >> nbuys;
 	for (int i = 0; i < nbuys; ++i) {
 		input >> c >> k >> p;
-		purchases[i] = { c, k, p };
-		index[c] = i;
+		if (index[c] == -1) {
+			index[c] = count; ++count;
+		}
+		purchase[index[c]] = k;
+		Offer o;
+		// price p for given 1 item
+		o.items[index[c]] = 1;
+		o.price = p;
+		offers.push_back(o);
 	}
 
 	dp();
 
-	output << minPrice[0] << std::endl;
+	output << minprice[purchase[0]][purchase[1]][purchase[2]][purchase[3]][purchase[4]] << std::endl;
 }
