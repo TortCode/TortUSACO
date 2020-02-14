2d259661fb37bd426719f99812448bb4eccca004
TortCode <44101297+TortCode@users.noreply.github.com>
2020-02-14 11:52:06 -0500

    Add files via upload

--- Section3.3/camelot.cpp
+++ Section3.3/camelot.cpp
@@ -22,19 +22,15 @@ vector<Position> knights;
 priority_queue<pair<int, Position>> pq;
 int rows, cols;
 
-int dist[MAXROW][MAXCOL];
+int dist[MAXROW][MAXCOL][MAXROW][MAXCOL];
 bool visited[MAXROW][MAXCOL];
 
 inline bool isValid(int r, int c)
 {
 	return r >= 0 && r < rows && c >= 0 && c < cols;
 }
-inline int index(Position p)
-{
-	return p.f * MAXCOL + p.s;
-}
 
-void dijkstra()
+void dijkstra(Position k)
 {
 	int moves[][2] =
 	{
@@ -43,11 +39,14 @@ void dijkstra()
 		{1,2}, {2,1},
 		{1,-2}, {-2,1}
 	};
-	for (int i = 0; i < MAXROW; ++i)
-		for (int j = 0; j < MAXCOL; ++j)
-			dist[i][j] = 1000;
-	dist[0][0] = 0;
-	pq.push({ 0, Position(0,0) });
+	for (int i = 0; i < rows; ++i)
+		for (int j = 0; j < cols; ++j)
+		{
+			visited[i][j] = false;
+			dist[k.f][k.s][i][j] = 1000;
+		}
+	dist[k.f][k.s][k.f][k.s] = 0;
+	pq.push({ 0, k });
 	while (!pq.empty()) {
 		Position p = pq.top().s; pq.pop();
 		if (visited[p.f][p.s]) continue;
@@ -55,10 +54,10 @@ void dijkstra()
 		for (int i = 0; i < 8; ++i) {
 			int nf = p.f + moves[i][0], ns = p.s + moves[i][1];
 			if (isValid(nf, ns) && !visited[nf][ns]) {
-				if (dist[p.f][p.s] + 1 < dist[nf][ns])
+				if (dist[k.f][k.s][p.f][p.s] + 1 < dist[k.f][k.s][nf][ns])
 				{
-					dist[nf][ns] = dist[p.f][p.s] + 1;
-					pq.push({ -dist[nf][ns], Position(nf,ns) });
+					dist[k.f][k.s][nf][ns] = dist[k.f][k.s][p.f][p.s] + 1;
+					pq.push({ -dist[k.f][k.s][nf][ns], Position(nf,ns) });
 				}
 			}
 		}
@@ -72,17 +71,19 @@ int min_moves(int r, int c)
 	for (Position k : knights) {
 		for (int i = -2; i <= 2; ++i) {
 			for (int j = -2; j <= 2; ++j) {
-				kingmove = min(kingmove,
-					max(abs(i),abs(j))
-					+ dist[abs(k.f - king.f - i)][abs(k.s - king.s - j)]
-					+ dist[abs(king.f + i - r)][abs(king.s + j - c)]
-					- dist[abs(k.f - r)][abs(k.s - c)]
+				int ni = king.f + i, nj = king.s + j;
+				if (isValid(ni, nj))
+					kingmove = min(kingmove,
+						max(abs(i), abs(j))
+						+ dist[r][c][ni][nj]
+						+ dist[ni][nj][k.f][k.s]
+						- dist[r][c][k.f][k.s]
 				);
 			}
 		}
-		ret += dist[abs(k.f - r)][abs(k.s - c)];
+		ret += dist[r][c][k.f][k.s];
 	}
-	ret = min(ret + kingmove, ret + max(abs(king.f-r), abs(king.s-c)));
+	ret = min(ret + kingmove, ret + max(abs(king.f - r), abs(king.s - c)));
 	return ret;
 }
 
@@ -99,7 +100,9 @@ int main()
 	king.second = c - 'A';
 	while (input >> c >> r)
 		knights.push_back({ r - 1, c - 'A' });
-	dijkstra();
+	for (int i = 0; i < rows; ++i)
+		for (int j = 0; j < cols; ++j)
+			dijkstra({ i,j });
 
 	int leastmove = -1;
 	int moves;
